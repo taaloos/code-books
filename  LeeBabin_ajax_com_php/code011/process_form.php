94d0aff0448b70a125cbc5050c6b36d9a97834fe
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-08-12 08:46:27 -0300

    corrigindo nome da pasta

---  LeeBabin_ajax_com_php/code011/process_form.php
+++
@@ -1,6 +0,0 @@
-<div style="padding: 10px;">
-	<p>The Results of your form submission are as follows:</p>
-	<p>Your Name: <span style="font-weight: bold;"><?=$_POST['yourname']?></span></p>
-	<p>Your Name: <span style="font-weight: bold;"><?=$_POST['youremail']?></span></p>
-	<p>Comments: <span style="font-weight: bold;"><?=$_POST['comment']?></span></p>
-</div>
---  LeeBabin_ajax_com_php/code012/sample12_3.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//sample12_3.php
-	if ($_POST['myvalue']){
-		//Clean the string to create the dynamic query.
-		$myvalue = mysql_real_escape_string ($_POST['myvalue']);
-		//You would now be clear to use this in a dynamic query such as:
-		$myquery = "SELECT something FROM somewhere WHERE somevalue='" . $myvalue . "'";
-	}
-?>
